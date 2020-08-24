b92e9ef462ada15f7da726f96a6cff894599bb61
kc-leung <chiu-leung@Hotmail.com>
2020-08-24 13:31:16 -0400

    login and register component sample

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi world!</h1>
-    </div>
+    <Router>
+      <Router exact path="/" component={Home} />
+      <Router exact path="/login" component={Login} />
+      <Router exact path="/register" component={Register} />
+    </Router>
   );
 }
 
